fad24256c5a6fb7f5710c494c08885e8d7f6c76b
Esteban7145 <125325981+Esteban7145@users.noreply.github.com>
2026-09-04 02:18:37 -0500

    Renovar cache para el menu de desplazamiento

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ipuc-villa-del-rio-v29-image-fit";
+const CACHE_NAME = "ipuc-villa-del-rio-v30-scroll-menu";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/css/styles.css", "/css/modern.css", "/assets/logo.png", "/assets/favicon.png", "/assets/ipuc-villa-del-rio-brand.png"];
 
 self.addEventListener("install", event => {
